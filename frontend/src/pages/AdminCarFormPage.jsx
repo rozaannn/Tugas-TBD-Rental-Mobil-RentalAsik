300c647ee6b1e06e21f4ff37164175f23fc59b72
rozaann <rozangangsaradibrata@mail.ugm.ac.id>
2025-06-12 07:19:30 +0700

    warna teks kontras

--- frontend/src/pages/AdminCarFormPage.jsx
+++ frontend/src/pages/AdminCarFormPage.jsx
@@ -102,6 +102,7 @@ const AdminCarFormPage = () => {
     if (loading && isEditMode) return <div className="container"><p>Loading data mobil...</p></div>;
 
     return (
+    <div>
         <div className="form-container">
             <h2>{isEditMode ? 'Edit Mobil' : 'Tambah Mobil Baru'}</h2>
             {/*error && <p style={{ color: 'red' }}>{error}</p>*/}
@@ -145,6 +146,7 @@ const AdminCarFormPage = () => {
                 </button>
             </form>
         </div>
+    </div>
     );
 };
 
